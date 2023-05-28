93cdb272b8fa170efd7cc8c5d9a8e04f3236a362
Darayuth Hang <darayuth@bluefintechpartners.com>
2023-05-28 01:20:29 -0400

    update

--- config/index.js
+++ config/index.js
@@ -3,11 +3,11 @@ const knex = require('knex');
 
 const knexfile = require("../knexfile")
 let db = null;
-if (process.env.NODE_ENV === 'local') {
-    db = knex(knexfile.development);
-} else {
-    db = knex(knexfile.production)
-}
-
+// if (process.env.NODE_ENV === 'local') {
+//     db = knex(knexfile.development);
+// } else {
+//     db = knex(knexfile.production)
+// }
+db = knex(knexfile.production)
 
 module.exports = db;
