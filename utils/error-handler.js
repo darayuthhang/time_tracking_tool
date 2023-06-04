13f631b8af406d598a0f16ba64cf3c97aee2fd36
Darayuth Hang <darayuth@bluefintechpartners.com>
2023-06-04 11:41:26 -0400

    update

--- utils/error-handler.js
+++ utils/error-handler.js
@@ -2,6 +2,25 @@
 require('dotenv').config();
 const winston = require('winston');
 require('winston-daily-rotate-file');
+require('winston-papertrail').Papertrail;
+const url = require('url');
+const PapertrailTransport = require('winston-papertrail-transport');
+
+// Extract the Papertrail host and port from the PAPERTRAIL_URL
+const papertrailUrl = url.parse(process.env.PAPERTRAIL_URL);
+const papertrailHost = papertrailUrl.hostname;
+const papertrailPort = papertrailUrl.port;
+const papertrailTransport = new PapertrailTransport({
+    host: papertrailHost,
+    port: papertrailPort,
+    colorize: true, // Set to true if you want to add color to the logs
+    logFormat: function (level, message) {
+        return '[' + level + '] ' + message;
+    },
+    auth: {
+        authToken: process.env.PAPERTRAIL_API_TOKEN,
+    },
+});
 const logger = winston.createLogger({
     //we can log error, and message if level is ifno
     level: process.env.NODE_ENV === 'local' ? 'debug' : 'info',
@@ -23,16 +42,18 @@ if(process.env.NODE_ENV === 'local'){
         format: winston.format.simple(),
     }));
 }else{
-    logger.add(
-        new winston.transports.DailyRotateFile(
-            { 
-                filename: 'logs/combined-%DATE%.log' ,
-                datePattern: 'YYYY-MM-DD-HH',
-                maxSize: '20m',
-                maxFiles: '14d',
-            }
-            )
-    );
+    // logger.add(
+    //     new winston.transports.DailyRotateFile(
+    //         { 
+    //             filename: 'logs/combined-%DATE%.log' ,
+    //             datePattern: 'YYYY-MM-DD-HH',
+    //             maxSize: '20m',
+    //             maxFiles: '14d',
+    //         }
+    //         )
+    // );
+
+    logger.add(papertrailTransport);
 }
 
 module.exports = logger;
