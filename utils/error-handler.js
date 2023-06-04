018b1295584177c255afdad0d4ab3ef5ea65570b
Darayuth Hang <darayuth@bluefintechpartners.com>
2023-06-04 11:56:05 -0400

    update

--- utils/error-handler.js
+++ utils/error-handler.js
@@ -3,24 +3,29 @@ require('dotenv').config();
 const winston = require('winston');
 require('winston-daily-rotate-file');
 require('winston-papertrail').Papertrail;
-const url = require('url');
-const PapertrailTransport = require('winston-papertrail-transport');
+// const url = require('url');
+// const PapertrailTransport = require('winston-papertrail-transport');
 
-// Extract the Papertrail host and port from the PAPERTRAIL_URL
-const papertrailUrl = url.parse(process.env.PAPERTRAIL_URL);
-const papertrailHost = papertrailUrl.hostname;
-const papertrailPort = papertrailUrl.port;
-const papertrailTransport = new PapertrailTransport({
-    host: papertrailHost,
-    port: papertrailPort,
-    colorize: true, // Set to true if you want to add color to the logs
-    logFormat: function (level, message) {
-        return '[' + level + '] ' + message;
-    },
-    auth: {
-        authToken: process.env.PAPERTRAIL_API_TOKEN,
-    },
-});
+// // Extract the Papertrail host and port from the PAPERTRAIL_URL
+
+
+// let papertrailUrl = "";
+// if (process.env.PAPERTRAIL_URL) {
+//     papertrailUrl = url.parse(process.env?.PAPERTRAIL_URL | "")
+// };
+// const papertrailHost = papertrailUrl?.hostname;
+// const papertrailPort = papertrailUrl?.port;
+// const papertrailTransport = new PapertrailTransport({
+//     host: papertrailHost,
+//     port: papertrailPort,
+//     colorize: true, // Set to true if you want to add color to the logs
+//     logFormat: function (level, message) {
+//         return '[' + level + '] ' + message;
+//     },
+//     auth: {
+//         authToken: process.env.PAPERTRAIL_API_TOKEN,
+//     },
+// });
 const logger = winston.createLogger({
     //we can log error, and message if level is ifno
     level: process.env.NODE_ENV === 'local' ? 'debug' : 'info',
@@ -42,6 +47,9 @@ if(process.env.NODE_ENV === 'local'){
         format: winston.format.simple(),
     }));
 }else{
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple(),
+    }));
     // logger.add(
     //     new winston.transports.DailyRotateFile(
     //         { 
@@ -53,7 +61,7 @@ if(process.env.NODE_ENV === 'local'){
     //         )
     // );
 
-    logger.add(papertrailTransport);
+   
 }
 
 module.exports = logger;
