90a1b8e9f798e20902c290776b580f92ffaef146
Darayuth Hang <darayuth@bluefintechpartners.com>
2023-06-12 17:55:54 -0400

    updatae

--- migrations/20230408145825_user.js
+++ migrations/20230408145825_user.js
@@ -42,32 +42,7 @@ exports.up = function (knex) {
         table.timestamp('created_at').defaultTo(knex.fn.now());
         table.timestamp('updated_at').defaultTo(knex.fn.now());
     })
-    .createTable('user_consent_phone_numbers', function (table) {
-        table.uuid('id').primary().defaultTo(knex.raw('(gen_random_uuid())'));
-        table.string('phone_number').notNullable();
-        table.string('country_code').notNullable(); //store without plus sign
-        table.boolean('consent').defaultTo(false).notNullable();
-        table.timestamp('created_at').defaultTo(knex.fn.now());
-        table.timestamp('updated_at').defaultTo(knex.fn.now());
-        table.uuid('user_id').references('id').inTable('users');
-    })
-    .createTable('user_consent_emails', function (table) {
-        table.uuid('id').primary().defaultTo(knex.raw('(gen_random_uuid())'));
-        table.string('email').notNullable();
-        table.boolean('consent').defaultTo(false).notNullable();
-        table.timestamp('created_at').defaultTo(knex.fn.now());
-        table.timestamp('updated_at').defaultTo(knex.fn.now());
-        table.uuid('user_id').references('id').inTable('users');
-    })
-    .createTable('schedules', function (table) {
-        table.uuid('id').primary().defaultTo(knex.raw('(gen_random_uuid())'));
-        table.date('schedule_date').notNullable();
-        table.string("schedule_time").notNullable();
-        table.timestamp('created_at').defaultTo(knex.fn.now());
-        table.timestamp('updated_at').defaultTo(knex.fn.now());
-        table.uuid('user_consent_email_id').references('id').inTable('user_consent_emails');
-        table.uuid('user_consent_phone_number_id').references('id').inTable('user_consent_phone_numbers');
-    })
+
     /**
      * user_consent_phone_numbers
      *  (
